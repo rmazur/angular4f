b7cba395632883098b44db1e6af3d79ee5eab749
Regis Mazur <regis_mazur@hotmail.com>
2017-04-01 14:11:51 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
+
+export const firebaseconfig = {
+    apiKey: "AIzaSyCF6qeZI5Da3wmLQnLgJu_TwOc2EaPkP58",
+    authDomain: "sweltering-heat-8181.firebaseapp.com",
+    databaseURL: "https://sweltering-heat-8181.firebaseio.com",
+    projectId: "sweltering-heat-8181",
+    storageBucket: "sweltering-heat-8181.appspot.com",
+    messagingSenderId: "990839630058"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseconfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
