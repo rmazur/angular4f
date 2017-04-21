7dec02bbfc6ab8ac93324a49276795bbea04ff73
Regis Mazur <regis_mazur@hotmail.com>
2017-04-21 23:07:15 +0100

    Change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { ArticleService } from './services/article.service';
 import { ArticleComponent } from './article/article.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
-import { MyGridApplicationComponent } from './my-grid-application/my-grid-application.component';
+// import { MyGridApplicationComponent } from './my-grid-application/my-grid-application.component';
 import { RedComponentComponent } from './red-component/red-component.component';
 import {RichGridComponent} from './rich-grid-example/rich-grid.component';
 import {DateComponent} from './date-component/date.component';
